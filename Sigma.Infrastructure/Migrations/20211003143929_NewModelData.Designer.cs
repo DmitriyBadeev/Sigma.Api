fbc19b77c2a6f120075d56e903e89845ed5ec2fd
DmitriyBadeev <dmitriibadeev@gmail.com>
2021-10-03 19:40:43 +0500

    Миграция новой модели

--- Sigma.Infrastructure/Migrations/20211003143929_NewModelData.Designer.cs
+++ Sigma.Infrastructure/Migrations/20211003143929_NewModelData.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Sigma.Infrastructure;
@@ -9,9 +10,10 @@ using Sigma.Infrastructure;
 namespace Sigma.Infrastructure.Migrations
 {
     [DbContext(typeof(FinanceDbContext))]
-    partial class FinanceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211003143929_NewModelData")]
+    partial class NewModelData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
