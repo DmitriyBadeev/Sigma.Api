dc794d946e19914eba8805c327593fb912021ad9
WAVEACCESS\Dmitry.Belikov <44783389+Dmitriy2Belikov@users.noreply.github.com>
2021-10-04 03:09:25 +0500

    Add hangfire assembly

---
+++ InvestIn.Hangfire/Filters/AuthorizationFilter.cs
@@ -0,0 +1,12 @@
+﻿using Hangfire.Dashboard;
+
+namespace InvestIn.Hangfire.Filters
+{
+    public class AuthorizationFilter : IDashboardAuthorizationFilter
+    {
+        public bool Authorize(DashboardContext context)
+        {
+            return true;
+        }
+    }
+}
---
+++ InvestIn.Hangfire/TaskRegistrationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Hangfire;
+using Hangfire.PostgreSql;
+using Microsoft.Extensions.Configuration;
+using Sigma.Integrations.Moex.Service;
+
+namespace InvestIn.Hangfire
+{
+    public class TaskRegistrationService
+    {
+        private readonly IMoexService _moexService;
+        private readonly IConfiguration _configuration;
+
+        public TaskRegistrationService(IMoexService moexService, IConfiguration configuration)
+        {
+            _moexService = moexService;
+            _configuration = configuration;
+        }
+
+        public void RegisterTasks()
+        {
+            JobStorage.Current = new PostgreSqlStorage(_configuration.GetConnectionString("HangfireConnection"));
+
+            RecurringJob.AddOrUpdate("MoexBoardRefresh", () => _moexService.RefreshBoards(), "0 0/5 * * * *");
+        }
+    }
+}
